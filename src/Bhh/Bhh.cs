b725fec21980b2bb1386266371062b8bf24ed50a
L4mbads <rizacal.mamen@gmail.com>
2025-03-24 09:18:44 +0700

    refactor: clean Bhh code

--- src/Bhh/Bhh.cs
+++ src/Bhh/Bhh.cs
@@ -7,51 +7,37 @@ using Robocode.TankRoyale.BotApi.Events;
 
 public class Bhh : Bot
 {
-    // The main method starts our bot
     static void Main(string[] args)
     {
         new Bhh().Start();
     }
 
-    public class ScannedBot
-    {
-        public double X;
-        public double Y;
-        public ScannedBot(ScannedBotEvent bot)
-        {
-            this.X = bot.X;
-            this.Y = bot.Y;
-
-        }
-    }
-
-    private bool foundBot;
-    private List<ScannedBot> scannedBots = new();
-    private ScannedBot scannedBot;
+    private bool foundBot = false;
     private int TurnDir = 1;
 
-    // Constructor, which loads the bot config file
     Bhh() : base(BotInfo.FromFile("Bhh.json")) { }
 
-    // Called when a new round is started -> initialize and do some movement
-    public override void Run()
+    public override void OnRoundStarted(RoundStartedEvent evt)
     {
         foundBot = false;
+    }
+    public override void Run()
+    {
+
         AdjustRadarForBodyTurn = false;
         AdjustGunForBodyTurn = false;
         AdjustRadarForGunTurn = false;
 
-        ScanColor = Color.FromArgb(0xFF, 0x8C, 0x00);   // Dark Orange
-        TurretColor = Color.FromArgb(0xFF, 0xA5, 0x00); // Orange
-        RadarColor = Color.FromArgb(0xFF, 0xD7, 0x00);  // Gold
-        BulletColor = Color.FromArgb(0xFF, 0x45, 0x00); // Orange-Red
-        BodyColor = Color.FromArgb(0xFF, 0xFF, 0x00);   // Bright Yellow
-        TracksColor = Color.FromArgb(0x99, 0x33, 0x00); // DarkBrownish - Orange
-        GunColor = Color.FromArgb(0xCC, 0x55, 0x00);    // Medium Orange
+        ScanColor = Color.FromArgb(0xFF, 0x8C, 0x00);
+        TurretColor = Color.FromArgb(0xFF, 0xA5, 0x00);
+        RadarColor = Color.FromArgb(0xFF, 0xD7, 0x00);
+        BulletColor = Color.FromArgb(0xFF, 0x45, 0x00);
+        BodyColor = Color.FromArgb(0xFF, 0xFF, 0x00);
+        TracksColor = Color.FromArgb(0x99, 0x33, 0x00);
+        GunColor = Color.FromArgb(0xCC, 0x55, 0x00);
 
         while (IsRunning)
         {
-            // Console.WriteLine(foundBot);
             if (!foundBot)
             {
                 WaitFor(new RadarTurnCompleteCondition(this));
@@ -60,22 +46,6 @@ public class Bhh : Bot
             else
             {
                 foundBot = false;
-                // ScannedBot evt = scannedBot;
-                // var bearing = BearingTo(evt.X, evt.Y);
-                // var gunBearing = GunBearingTo(evt.X, evt.Y);
-                // Console.WriteLine(bearing);
-                // var distance = DistanceTo(evt.X, evt.Y);
-                // var speed = Math.Abs(bearing) < 20 ? 50 : 20;
-                // SetForward(speed);
-                // SetTurnLeft(bearing);
-                // SetTurnGunLeft(gunBearing);
-                // SetTurnRadarRight(0);
-                // SetTurnRadarLeft(RadarTurnRemaining * TurnDir);
-                // TurnDir *= -1;
-                // if (distance < 300)
-                // {
-                //     SetFire(1);
-                // }
                 Rescan();
             }
 
@@ -83,7 +53,7 @@ public class Bhh : Bot
         }
     }
 
-    private void distanceFireGun(double distance)
+    private void SmartFire(double distance)
     {
         double powerFire;
         if (distance < 200)
@@ -106,27 +76,29 @@ public class Bhh : Bot
     }
 
 
-    // We saw another bot -> fire!
     public override void OnScannedBot(ScannedBotEvent evt)
     {
-        // scannedBots.Add(new ScannedBot(evt));
-        // scannedBot = new ScannedBot(evt);
+        // RAMMMMMM
+
         var bearing = BearingTo(evt.X, evt.Y);
         var gunBearing = GunBearingTo(evt.X, evt.Y);
-        // Console.WriteLine(bearing);
         var distance = DistanceTo(evt.X, evt.Y);
         var speed = Math.Abs(bearing) < 20 ? 50 : 20;
+
         SetForward(speed);
         SetTurnLeft(bearing);
         SetTurnGunLeft(gunBearing);
         SetTurnRadarRight(0);
         SetTurnRadarLeft(RadarTurnRemaining * TurnDir);
+
         TurnDir *= -1;
+
+        // SHOOOOOOT
         if (gunBearing < 10)
         {
-            distanceFireGun(distance);
-            // SetFire(1);
+            SmartFire(distance);
         }
+
         Go();
         foundBot = true;
     }
@@ -140,35 +112,11 @@ public class Bhh : Bot
 
     }
 
-    // We collided with a wall -> reverse the direction
     public override void OnHitWall(HitWallEvent e)
     {
         TurnRight(180);
-        // if (movingForward)
-        // {
-        //     SetBack(40000);
-        //     movingForward = false;
-        // }
-        // else
-        // {
-        //     SetForward(40000);
-        //     movingForward = true;
-        // }
     }
-    public class TurnCompleteCondition : Condition
-    {
-        private readonly Bot bot;
 
-        public TurnCompleteCondition(Bot bot)
-        {
-            this.bot = bot;
-        }
-
-        public override bool Test()
-        {
-            return bot.TurnRemaining == 0;
-        }
-    }
     public class RadarTurnCompleteCondition : Condition
     {
         private readonly Bot bot;
